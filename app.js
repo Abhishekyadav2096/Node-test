73bedc17647ea92625f76d4ff8b7ecdbbca7db45
Abhishekyadav2096 <104411173+Abhishekyadav2096@users.noreply.github.com>
2023-08-08 10:55:07 +0530

    Update app.js
    app changes

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  return res.send("ABHISHEK YADAV DEV");
+  return res.send("ABHISHEK YADAV DEVLOPER");
 });
 
 app.listen(3000, () => {
